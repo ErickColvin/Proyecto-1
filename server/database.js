23a38af1ec779f6b147074bbcc68cc3a7353e92c
birira <vicente.pereira.ch@gmail.com>
2025-05-29 20:42:25 -0400

    db URI

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const URI = "mongodb://localhost:27017"
+const URI = "mongodb+srv://benjaminberrios:ds3P808U5eZhY8Kx@cluster0.4h6c4n8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(URI)
 
